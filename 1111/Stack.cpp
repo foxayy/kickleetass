dfa97345c8653b3aedd0d1cf6902e87414a31902
louis <gao527950554@foxmail.com>
2020-04-07 22:45:11 +0800

    refactor:添加题目及解析链接

--- 1111/Stack.cpp
+++ 1111/Stack.cpp
@@ -5,8 +5,17 @@
 #include <unordered_map>
 #include <queue>
 using namespace std;
+
+
+// 1111. 有效括号的嵌套深度
+// 来源：力扣（LeetCode）
+// 链接：https://leetcode-cn.com/problems/maximum-nesting-depth-of-two-valid-parentheses-strings
+// 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+
+// 题解参考链接：https://leetcode-cn.com/problems/maximum-nesting-depth-of-two-valid-parentheses-strings/solution/you-xiao-gua-hao-de-qian-tao-shen-du-by-leetcode-s
 class Solution {
 public:
+
 	vector<int> maxDepthAfterSplit(string seq) {
 		int d = 0,i=0;
         vector<int> ans(seq.size());
