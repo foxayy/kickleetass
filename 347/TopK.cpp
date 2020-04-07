06e4ef9d528542b4e834dda65046bb444ee4eff3
louis <gao527950554@foxmail.com>
2020-04-07 22:33:16 +0800

    refactor:添加题目描述及注释

--- 347/TopK.cpp
+++ 347/TopK.cpp
@@ -9,6 +9,23 @@
 
 #include <algorithm>
 
+// 347. 前 K 个高频元素
+// 给定一个非空的整数数组，返回其中出现频率前 k 高的元素。
+// 示例 1:
+// 输入: nums = [1,1,1,2,2,3], k = 2
+// 输出: [1,2]
+//
+// 示例 2:
+// 输入: nums = [1], k = 1
+// 输出: [1]
+// 说明：
+// 	你可以假设给定的 k 总是合理的，且 1 ≤ k ≤ 数组中不相同的元素的个数。
+// 	你的算法的时间复杂度必须优于 O(n log n) , n 是数组的大小。
+// 来源：力扣（LeetCode）
+// 链接：https://leetcode-cn.com/problems/top-k-frequent-elements
+// 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+
+
 //TopK问题
 //思路：哈希表 + 限制优先队列（堆）
 //时间复杂度：O(NlogK)
@@ -22,17 +39,23 @@ public:
 		}
 	};
 	vector<int> topKFrequent(vector<int>& nums, int k) {
+
+		//统计整数对应出现次数，O（N）
 		unordered_map<int, int> dicts;
 		for (const int& d : nums) {
 			dicts[d]++;
 		}
+
+		//建立优先队列（小根堆），O（NlogK）
 		priority_queue<pair<int,int>, vector<pair<int, int>>,cmp> topK;
 		for (auto it : dicts) {
 			topK.push(it);
-			if (topK.size() > k) {
+			if (topK.size() > k) {//堆的大小超过K，移除当前最小值
 				topK.pop();
 			}
 		}
+
+		//读出结果
 		vector<int> res(k);
 		while (!topK.empty()) {
 			res[--k] = topK.top().first;
